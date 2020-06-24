7581258bb7413e3c0287437f874fcb8ab7d8cbed
Yoojae <yooflyhigh@gmail.com>
2020-06-24 23:18:38 +0900

    데이터 삽입 구현중 에러 발생

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,14 +13,18 @@ export class HomePage {
     private router: Router,
   ) {
     this.boardList.push(
-      { num: 0, tag: '[동물]', title: '과자 남은 거 좀 있나', contents: '사진', date: '6-24-13:00', hit: 23},
-      { num: 1, tag: '[엽기]', title: '나도 웃겨', contents: '글글글글글', date: '6-25-7:59', hit: 2},
-      { num: 3, tag: '[정보]', title: '유튜브 광고 기준', contents: '동영상', date: '6-26-13:33', hit: 36},
-      { num: 4, tag: '[감동]', title: '어머니가 주신...', contents: '솜사탕...', date: '6-26-12:52', hit: 85},
-      { num: 5, tag: '[사회]', title: '북한 도발', contents: '북한이 삐라를 ~~ ', date: '6-27-17:16', hit: 105},
+      { num: 4, tag: '[동물]', title: '과자 남은 거 좀 있나', contents: '사진', date: '6-24-13:00', hit: 23},
+      { num: 3, tag: '[엽기]', title: '나도 웃겨', contents: '글글글글글', date: '6-25-7:59', hit: 2},
+      { num: 2, tag: '[정보]', title: '유튜브 광고 기준', contents: '동영상', date: '6-26-13:33', hit: 36},
+      { num: 1, tag: '[감동]', title: '어머니가 주신...', contents: '솜사탕...', date: '6-26-12:52', hit: 85},
+      { num: 0, tag: '[사회]', title: '북한 도발', contents: '북한이 삐라를 ~~ ', date: '6-27-17:16', hit: 105},
     )
   }
   moveToPage(pagename: String){
     this.router.navigate(['home', pagename]);
+  }
+
+  public addBoardList(addList: any[]){
+    this.boardList.push(addList);
   }
 }
--- src/app/home/writing/writing.page.ts
+++ src/app/home/writing/writing.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HomePage } from '../home.page';
 
 @Component({
   selector: 'app-writing',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./writing.page.scss'],
 })
 export class WritingPage implements OnInit {
-
-  constructor() { }
-
+  static num: number;
+  tag: string;
+  title: string;
+  contents: string;
+  date: string;
+  hit: number;
+  
+  
+  constructor(private BoardList: HomePage, ) { 
+    WritingPage.num = 5;  
+  }
   ngOnInit() {
   }
 
+  submit(){
+    let data: any = [];
+    data.push(WritingPage.num++);
+    data.push(this.title);
+    data.push(this.contents);
+
+    console.log(data);
+    this.BoardList.addBoardList(data);
+    
+  }
 }
